1ef6420a3149901ee1ec31bf5356c8d05e70f0f6
MagnusVinther <magn790m@elev.techcollege.dk>
2022-09-20 15:16:02 +0200

    HomeBanner done

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 
 import { Layout } from "../../App/Layout/Layout"
 
+// Komponenter
+import { HomeBanner } from "./HomeBanner"
+
+
 export const Home = () => {
     return (
         <Layout title="Forside" description="Her er forsiden">
-            <p>Hej med dig forside.</p>
+
+            <HomeBanner />
         </Layout>
     )
 }
---
+++ src/components/Pages/Home/HomeBanner.jsx
@@ -0,0 +1,63 @@
+
+// Hooks
+import { useEffect, useState } from 'react'
+
+// Fetch
+import axios from "axios"
+
+// Styles
+import Styles from "./HomeBanner.module.scss"
+
+// Function Component til Home banner
+export const HomeBanner = () => {
+    // Destructure af data. Usestate skal finde et array.
+    const [data, setData] = useState([])
+
+    // UseEffect hook til at styre rendering af siden.
+    useEffect(() => {
+        // Vi skal fange data fra api. Gøres med en asynkron funktion.
+        const getData = async () => {
+            // try/catch til error handling
+            try {
+                // Variable med get resultat udtræk fra api. Skal kun bruge 1 udtræk og bruger limit=1 ( Query parameter )
+                const result = await axios.get('https://api.mediehuset.net/detutroligeteater/events?limit=1')
+                // setter data tilstand.
+                setData(result.data.items)
+            } 
+            catch (err) {
+                console.log(err)
+                // for at fortælle hvor fejlen sker
+                console.log("der er fejl i homebanner fetch")
+            }
+        }
+        // Funktionskald.
+        getData()
+    // Dependancy array til at fortælle hvilke forandringer der skal lave en rendering.
+    }, [setData])
+
+    // Returnering i DOM
+    return (
+        // laver en section til banner
+        <section className={Styles.sectionBanner}>
+            {/* mapper data */}
+            {data && data.map((item) => {
+                return (
+                    // Indsætter data i en figure med figcaption.
+                    <figure key={item.id}>
+                        <figcaption>
+                            <article>
+                                {/* inline-styling */}
+                                <p style={{color: '#808080'}}>{item.stage_name}</p>
+                                <b >{item.startdate} - {item.stopdate}</b>
+                                <hr />
+                                <h2>{item.title}</h2>
+                                <p style={{color: '#707070', fontSize: '2rem'}}>{item.genre}</p>
+                            </article>
+                        </figcaption>
+                        <img src={item.image} alt={item.title} loading="lazy" />
+                    </figure>
+                )
+            })}
+        </section>
+    )
+}
